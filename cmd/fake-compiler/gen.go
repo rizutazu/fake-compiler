9aea076c1c3e35d1212d62cd7f8cdda72cff4052
rizutazu <96570586+rizutazu@users.noreply.github.com>
2025-04-23 21:50:25 +0800

    gosh, I forgot something

--- cmd/fake-compiler/gen.go
+++ cmd/fake-compiler/gen.go
@@ -15,35 +15,34 @@ var genCmd = &cobra.Command{
 	Long: `iterate through given directory by given compiler type, and then generate corresponding config file, which 
 can be used for generating fake compile logs, so that the directory is no longer needed`,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		var c cc.Compiler
 		dirPath, err := util.FormatPath(dirPath)
 		if err != nil {
 			log.Fatal(err)
 		}
-		switch compiler {
+		switch compilerType {
 		case "cxx":
 			c, err = cc.NewCXXCompiler(dirPath, cc.SourceTypeDir, 1)
 			if err != nil {
 				log.Fatal(err)
 			}
 		default:
-			log.Fatalf("unknown compiler type %s\n", compiler)
+			log.Fatalf("unknown compiler type %s\n", compilerType)
 		}
 		err = c.DumpConfig(outputPath)
 		if err != nil {
 			log.Fatal(err)
 		}
 		info, _ := os.Stat(outputPath)
-		fmt.Printf("Output: %s\nType: %s\nSize: %.1f KiB\n", outputPath, compiler, float64(info.Size())/1024)
+		fmt.Printf("Output: %s\nType: %s\nSize: %.1f KiB\n", outputPath, compilerType, float64(info.Size())/1024)
 	},
 }
 
 func init() {
-	genCmd.Flags().StringVarP(&compiler, "compiler", "C", "", "specified compiler type")
+	genCmd.Flags().StringVarP(&compilerType, "compiler", "C", "", "specified compiler type")
 	genCmd.Flags().StringVarP(&dirPath, "dir", "d", "", "path of directory to compile")
 	genCmd.Flags().StringVarP(&outputPath, "output", "o", "", "config file output path")
-	_ = genCmd.MarkFlagRequired("compiler")
+	_ = genCmd.MarkFlagRequired("compilerType")
 	_ = genCmd.MarkFlagRequired("dir")
 	_ = genCmd.MarkFlagRequired("output")
 }
--- cmd/fake-compiler/main.go
+++ cmd/fake-compiler/main.go
@@ -6,25 +6,25 @@ import (
 )
 
 // exclusive:
-// run -f configPath
+// run -c configPath
 // run -d dirPath
 
 // persistent:
-// run -t threads -c compiler -p progressbar
+// run -t threads -C compilerType -p progressbar
 
 // persistent:
-// gen -c compiler -d dirPath -o output path
+// gen -C compilerType -d dirPath -o output path
 
 var configPath string
 var dirPath string
 var threads int
-var compiler string
+var compilerType string
 
-// var bar string
+// var bar string	// todo: idk
 var outputPath string
 
 var rootCmd = &cobra.Command{
-	Use:   "fake-compile",
+	Use:   "fake-compiler",
 	Short: "pretend as if it is compiling something",
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := cmd.Help(); err != nil {
@@ -38,33 +38,8 @@ func init() {
 	rootCmd.AddCommand(genCmd)
 }
 
-const THREADS int = 16
-
 func main() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
-	//var path string
-	//if len(os.Args) < 2 {
-	//	var err error
-	//	path, err = os.Getwd()
-	//	if err != nil {
-	//		log.Fatal("get current working directory failed")
-	//	}
-	//} else {
-	//	path = os.Args[1]
-	//}
-	//
-	//path, err := util.FormatPath(path)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//c, err := compiler.NewCXXCompiler(path, compiler.SourceTypeDir, THREADS)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//c.Run()
-
 }
