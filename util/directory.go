492583cbb0426a0b7ff4b56d23bbaf91e5218775
rizutazu <96570586+rizutazu@users.noreply.github.com>
2026-02-21 19:33:32 +0800

    add symlink check to avoid endless recursion during directory traverse

--- util/directory.go
+++ util/directory.go
@@ -3,9 +3,7 @@ package util
 import (
 	"errors"
 	"os"
-	"path/filepath"
 	"regexp"
-	"strings"
 	"sync"
 )
 
@@ -26,10 +24,8 @@ type Directory struct {
 	Complete bool
 	//Parent   *Directory // todo
 
-	isRoot         bool
 	filenameFilter *regexp.Regexp
 	ignoreEmptyDir bool
-	canRead        bool
 }
 
 type File struct {
@@ -67,7 +63,6 @@ func NewDirectory(path, filenameFilter string, ignoreEmptyDir bool) (*Directory,
 		Path:           path,
 		filenameFilter: filter,
 		ignoreEmptyDir: ignoreEmptyDir,
-		isRoot:         true,
 		Complete:       false,
 	}, nil
 }
@@ -98,12 +93,25 @@ func (directory *Directory) traverse(path string, wg *sync.WaitGroup) {
 	clear(directory.SubDirs)
 	clear(directory.Files)
 	directory.Path = path
-	files, err := os.ReadDir(path)
-	if errors.Is(err, os.ErrPermission) {
-		directory.canRead = false
-	} else {
-		directory.canRead = true
+
+	// check if is symlink
+	// don't handle symlink
+	info, err := os.Stat(path)
+	if err != nil {
+		directory.Complete = true
+		return
 	}
+	if info.Mode()&os.ModeSymlink != 0 {
+		directory.Complete = true
+		return
+	}
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		directory.Complete = true
+		return
+	}
+
 	for _, file := range files {
 		if file.IsDir() {
 			subDir := new(Directory)
@@ -131,97 +139,4 @@ func (directory *Directory) traverse(path string, wg *sync.WaitGroup) {
 		}
 	}
 	directory.Complete = true
-}
-
-// ├ ── │ └
-
-// String provides a `tree` command-like view of the file architecture,
-// with files come the first, then the subdirectories
-//
-// example:
-// /tmp/dir
-// ├── file1
-// ├── file2
-// ├── dir1
-// │   ├── file3
-// │   └── file4
-// ├── dir2
-// │   └──
-// └── dir3: permission denied
-func (directory *Directory) String() string {
-	if directory == nil || !directory.Complete {
-		return ""
-	}
-
-	s := strings.Builder{}
-	fileCount := len(directory.Files)
-	subDirCount := len(directory.SubDirs)
-
-	// write directory name without newline
-	if directory.isRoot {
-		s.WriteString(directory.Path)
-	} else {
-		s.WriteString(filepath.Base(directory.Path))
-	}
-
-	// show "permission denied"
-	if directory.canRead {
-		s.WriteString("\n")
-	} else {
-		s.WriteString(": permission denied\n")
-		return s.String()
-	}
-
-	// write files
-	switch {
-	case fileCount == 0:
-		if subDirCount == 0 {
-			s.WriteString("└──\n")
-		}
-		// else: write nothing
-	case fileCount == 1:
-		if subDirCount == 0 {
-			s.WriteString("└── " + directory.Files[0].Name + "\n")
-		} else {
-			s.WriteString("├── " + directory.Files[0].Name + "\n")
-		}
-
-	default:
-		for i, file := range directory.Files {
-			if i != fileCount-1 {
-				s.WriteString("├── " + file.Name + "\n")
-			} else {
-				if subDirCount == 0 {
-					s.WriteString("└── " + file.Name + "\n")
-				} else {
-					s.WriteString("├── " + file.Name + "\n")
-				}
-			}
-		}
-	}
-
-	// recursively write directory
-	for i, subDir := range directory.SubDirs {
-		lines := strings.Split(subDir.String(), "\n")
-		for j, line := range lines {
-			if line == "" {
-				continue
-			}
-			if i == subDirCount-1 { // last directory entry
-				if j == 0 { // the directory name part: last to draw a "tree branch"
-					s.WriteString("└── " + line + "\n")
-				} else {
-					s.WriteString("    " + line + "\n")
-				}
-			} else { // other directory entry
-				if j == 0 {
-					s.WriteString("├── " + line + "\n")
-				} else {
-					s.WriteString("│   " + line + "\n")
-				}
-			}
-
-		}
-	}
-	return s.String()
 }
